9843bb93c7e315796cadf9ddb1a387472646c327
Jennygp17 <107749651+Jennygp17@users.noreply.github.com>
2022-08-07 16:39:20 +0100

    Added API calls to forecast in JS

--- src/index.js
+++ src/index.js
@@ -21,22 +21,45 @@ if (minute < 10) {
 }
 todaysDate.innerHTML = `${day}, ${hour}:${minute}`;
 
+function formatForecastDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#weatherForecast");
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
+
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
                   <div class="col-2">
-            <div class="weekDay">${day}</div>
-            <img src="Media/sun.png" alt="sunny" id="image" width="70px" />
+            <div class="weekDay">${formatForecastDay(forecastDay.dt)}</div>
+            <img src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png" alt="${
+          forecastDay.weather[0].description
+        }" id="image" width="70px" />
             <div class="temps">
-              High | 19°</div>
-              <div class="lTemps">Low | 13°</div>
+              High | ${Math.round(forecastDay.temp.max)}°</div>
+              <div class="lTemps">Low | ${Math.round(
+                forecastDay.temp.min
+              )}°</div>
             </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
